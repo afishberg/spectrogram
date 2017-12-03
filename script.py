5e5745c09c20f7d9b8d3e942f17b5751d951b29d
afishberg <fishberg.github@gmail.com>
2017-12-03 00:45:46 -0500

    added allpole

--- script.py
+++ script.py
@@ -40,9 +40,6 @@ def time_alias(dat,n):
 def td_dft(dat, winlen, winoverlap, dftsize, winty='rect'):
     views = view_as_windows(dat,winlen, winlen-winoverlap)
 
-    if winlen > dftsize:
-        views = [ time_alias(v,dftsize) for v in views ]
-
     if winty == 'hamming':
         win = hamming(winlen)
     elif winty == 'blackman':
@@ -50,7 +47,13 @@ def td_dft(dat, winlen, winoverlap, dftsize, winty='rect'):
     else:
         win = np.array(winlen*[1])
 
-    dfts = [ fftshift(fft(v*win,dftsize)) for v in views ]
+    views = [ v*win for v in views ]
+
+    if winlen > dftsize:
+        views = [ time_alias(v,dftsize) for v in views ]
+
+
+    dfts = [ fftshift(fft(v,dftsize)) for v in views ]
 
     return np.array(dfts)
 
@@ -96,6 +99,20 @@ def power_est(spec):
     pgram = np.sum(spec,axis=0) / spec.shape[0]
     return pgram
 
+def allpole_est(dat,order):
+    rss = [ np.correlate(dat[i:],np.conj(dat))[0] for i in range(order+1) ]
+    print(rss)
+    automat = [ [ rss[abs(i-j)] for j in range(order) ] for i in range(order) ]
+
+    mat = np.linalg.inv(np.array(automat))
+    vec = np.array(rss[1:])
+    alpha = np.matmul(mat,vec)
+    w,h = scipy.signal.freqz([1],[1]+list(-1*alpha),whole=True)
+
+    h = np.array(list(h[len(h)//2:]) + list(h[:len(h)//2]))
+    plt.plot(w/pi-1,2*dB(h))
+    plt.show()
+
 def noise_est(spec, percent):
     sort = np.sort(spec.flatten())
     subset = sort[:int(len(sort)*percent)]
@@ -140,12 +157,54 @@ def process(samples,occ,eta):
 # ==============================================================================
 
 def chirp_sample(a,dur,fs):
-    t = np.arange(0,dur,1/fs)
-    sig = np.cos( a*(t*t) )
+    t = np.arange(0,2000)
+    sig = np.cos( a*t*t )
+
+    plt.plot(sig)
+    plt.show()
+
+    dft = fftshift(fft(sig))
+    mag = np.abs(dft)
+    plt.plot(mag)
+    plt.show()
+
     return sig
+
+#p814
+def chirp_test():
+    t = np.arange(0,5,1/256000)
+    a = 2*pi
+    print(t)
+    sig = np.cos( a*t*t )
+
+    plt.plot(sig)
+    plt.show()
+
+    dft = fftshift(fft(sig))
+    mag = np.abs(dft)
+    plt.plot(mag)
+    plt.show()
 
 def ld_file(infile):
     return scipy.fromfile(open(infile), dtype=scipy.complex64)
+
+def builtin_spectrogram(infile):
+    dat = ld_file('blind_test_project02.raw')
+    dat = np.abs(dat)
+    f,t,Sxx = scipy.signal.spectrogram(
+        dat,
+        fs=2048000,
+        window=blackman(4096),
+        noverlap=2048,
+        nfft=4096,
+        scaling='spectrum',
+        mode='magnitude'
+    )
+
+    plt.pcolormesh(t, f, 2*dB(Sxx))
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
 
 # ==============================================================================
 # Main Function
@@ -154,8 +213,14 @@ def ld_file(infile):
 if __name__ == '__main__':
     # data, percent noise estimate, eta
     #process(ld_file('blind_test.raw'), .5, 8)
-    process(ld_file('blind_test_project02.raw'), .8, 7)
-    #process(chirp_sample(pi,5,256000), .8, 3)
+    #process(ld_file('blind_test_project02.raw'), .8, 7)
+    #process(chirp_sample(15*pi*10**-6,5,256000), .8, 3)
+    #chirp_test()
+    #chirp_sample(15*pi*10**-6,5,256000)
+
+    builtin_spectrogram('blind_test_project02.raw')
+    dat = ld_file('blind_test_project02.raw')
+    allpole_est(dat,10)
 
 # ==============================================================================
 
